75039c2f83c8aa7bfeffb4ce723038ccaad49c38
Alex <alex.mahinko@gmail.com>
2017-04-03 13:24:12 -0400

    methods work and interact correctly. Broken line commented out and explained.

--- people.rb
+++ people.rb
@@ -4,3 +4,12 @@ require "./Instructor.rb"
 
 christina = Student.new("Christina")
 chris = Instructor.new("Chris")
+
+puts chris.greeting
+puts christina.greeting
+
+puts chris.teach
+puts christina.learn
+
+# puts christina.teach  #<- Won't work, as the teach and learn methods are undefined for christina
+# puts chris.learn           and Chris respectively and ergo aren't inherited.
